46d46e4a1cc1e696944131858f8c69f3c8c0b42d
İrem Akyol <69118634+lastirembender@users.noreply.github.com>
2024-05-18 12:10:21 +0000

    bug fixed

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 filename=$1
 email_matches=$(grep -i "@amazon.com" "$filename")
-name_surname=$(echo "$email_matches" | awk -F ',' '{printf("%s %s ", $3, $2)} END {print ""}')
+name_surname=$(echo "$email_matches" | awk -F ',' '{printf("%s %s \n", $3, $2)}')
 echo "$name_surname" >> output_names.txt 
