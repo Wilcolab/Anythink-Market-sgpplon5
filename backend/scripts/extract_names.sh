1757a49f638094e8dcd4ac53566e817998c1678e
WilcoApp <hello@wilco.gg>
2024-05-18 16:59:35 +0000

    Initial commit

--- backend/scripts/extract_names.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-filename=$1
-output_filename=$2
-name_surname=$(grep -i "@amazon.com" "$filename" | awk -F ',' '{printf("%s %s\n", $3, $2)}')
-echo "$name_surname" >> "$output_filename"
